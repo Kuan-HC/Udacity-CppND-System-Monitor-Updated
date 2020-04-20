bfe047191c3a76d7ba89abb061779e594bbab05e
Kuan-HC <blackshield33@gmail.com>
2020-04-20 12:35:30 +0000

    up time passed

--- src/format.cpp
+++ src/format.cpp
@@ -1,27 +1,29 @@
 #include <string>
-
+#include <iomanip>  /* Fill number with digit 0 */
 #include "format.h"
+
+#define MINSEC 60;
+#define HOURMIN 60;
+#define HOURSEC 3600;
 
 using std::string;
 
-// TODO: Complete this helper function
+// TODO: (Done) Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds)
-{
-    const int SECPERMIN =60;
-    const int SECPERHOUR =3600;
-     
-    long lseconds = seconds % SECPERMIN;
-    long lminutes = (seconds / SECPERMIN) %SECPERMIN;
-    long lhours   = seconds /SECPERHOUR; 
+{     
+    unsigned int seconds_ = seconds % MINSEC;
+    unsigned int minutes_ = seconds % HOURSEC;
+    minutes_ /= HOURMIN;
+    unsigned int hours_   = seconds / HOURSEC; 
 
-    std::stringstream ssresult;
+    std::stringstream output;
 
-    ssresult << std::setfill('0') << std::setw(2) << lhours << ":";
-    ssresult << std::setfill('0') << std::setw(2) << lminutes << ":";  
-    ssresult << std::setfill('0') << std::setw(2) << lseconds; 
+    output << std::setw(2) << std::setfill('0') << hours_ << ":";
+    output << std::setw(2) << std::setfill('0') <<  minutes_ << ":";  
+    output << std::setw(2) << std::setfill('0') << seconds_; 
 
-    return ssresult.str();
+    return output.str();
 }
